bebfecf212874acae84ec1a6e7c84d9f57efe3aa
Joe Masu <josephmuhindo089@gmail.com>
2024-04-24 03:49:58 -0700

    MealAPI

---
+++ 01myapp3/src/Main.js
@@ -0,0 +1,31 @@
+import axios from "axios"
+import {useState, useEffect} from "react";
+import "./style.css";
+
+function Main(){
+    const [items, setItems]= useState([]);
+    useEffect(() => {
+        axios
+          .get("https://www.themealdb.com/api/json/v1/1/filter.php?c=Seafood")
+          .then((res) => {
+            // console.log(res.data);
+            setItems(res.data.meals);
+          })
+          .catch((err) => {
+            console.log(err);
+          });
+    }, []);
+    const itemsList = items.map(({strMeal, strMealThumb, idMeal}) =>{
+        return(
+            <section className="card">
+                <img src={strMealThumb} alt=""></img>
+                <section className="content">
+                <p>{strMeal}</p>
+                <p>#{idMeal}</p>
+                </section>
+            </section>
+        )
+    })
+    return <div className="items-container">{itemsList}</div>
+}
+export default Main;
