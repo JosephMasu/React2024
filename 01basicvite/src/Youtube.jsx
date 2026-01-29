be254af9b7957423e8cdec4ac26daba180f1d8d1
Joseph Masu <60499662+JosephMasu@users.noreply.github.com>
2026-01-29 23:44:22 +0300

    Update Youtube.jsx

--- 01basicvite/src/Youtube.jsx
+++ 01basicvite/src/Youtube.jsx
@@ -1,6 +1,5 @@
 function Youtube(){
     return(
-    <h1>Hey!, this is my first Youtube react App</h1>)
-        
+    <h1>Hey!, this is my first Youtube react App</h1>)       
 }
 export default Youtube
